9d7cc617b6263b3d71b9c3c9d47c5eb55be31eec
Pedro Ferreira de Matos <elitdruid@gmail.com>
2019-07-06 13:18:01 +0100

    Second service created

---
+++ series-service/src/main/java/com/example/series/bean/NetflixSerie.java
@@ -0,0 +1,46 @@
+package com.example.series.bean;
+
+public class NetflixSerie {
+    private String name;
+    private int seasons;
+    private boolean netflixOriginal;
+
+    public NetflixSerie(String name, int seasons, boolean netflixOriginal) {
+        this.name = name;
+        this.seasons = seasons;
+        this.netflixOriginal = netflixOriginal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public boolean isNetflixOriginal() {
+        return netflixOriginal;
+    }
+
+    public void setNetflixOriginal(boolean netflixOriginal) {
+        this.netflixOriginal = netflixOriginal;
+    }
+
+    @Override
+    public String toString() {
+        return "NetflixSerie{" +
+                "name='" + name + '\'' +
+                ", seasons=" + seasons +
+                ", netflixOriginal=" + netflixOriginal +
+                '}';
+    }
+}
