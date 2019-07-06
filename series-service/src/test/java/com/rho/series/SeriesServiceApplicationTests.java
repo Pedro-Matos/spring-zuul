b5af3e8ae0d90785c5ec63fa9a4cf76d5ec209ca
Pedro Ferreira de Matos <elitdruid@gmail.com>
2019-07-06 14:49:59 +0100

    Small changes... Refactoring, descriptions, packages names...

--- series-service/src/test/java/com/rho/series/SeriesServiceApplicationTests.java
+++ series-service/src/test/java/com/rho/series/SeriesServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.zuulserver;
+package com.rho.series;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZuulServerApplicationTests {
+public class SeriesServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
--- zuul-server/src/main/java/com/rho/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/rho/zuulserver/ZuulServerApplication.java
@@ -1,15 +1,17 @@
-package com.example.series;
+package com.rho.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SeriesServiceApplication {
+@EnableZuulProxy
+public class ZuulServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeriesServiceApplication.class, args);
+		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 
 }
